12b7f3c157f57996f6de15193c16d0b8bfef23a8
cbrugel <camille.brugel@laposte.net>
2015-08-16 17:21:10 +0200

    [WIP] bucket struct + insertion + rehash

---
+++ RobinHoodHashTable.hpp
@@ -0,0 +1,124 @@
+#ifndef __ROBIN_HOOD_HASH_TABLE_H__
+#define __ROBIN_HOOD_HASH_TABLE_H__
+
+#include <functional>
+#include <cstddef>
+#include <vector>
+
+/**
+ * This is an implementation of a Robin-Hood hashing based hashtable, you can find more info there :
+ * http://codecapsule.com/2013/11/11/robin-hood-hashing/
+ **/
+
+
+/**
+ *Bucket: one entry of the hashtable
+ */
+template <typename T>
+struct Bucket
+{
+    typedef T value_type;
+
+    constexpr static std::size_t EMPTY  = static_cast<std::size_t>(-1);
+
+    void markEmpty() { _dib = EMPTY; }
+    bool isEmpty() const { return _dib == EMPTY; }
+
+    std::size_t _dib = EMPTY;   // Distance to Initial Bucket
+    T           _value;         // the actual value
+};
+
+
+typedef int T; //TODO comment
+
+
+/**
+ * RobinHoodHashTable: hashtable using the Robin Hood hashing
+ */
+template <// TODO uncomment typename T,                               // type of the contained values
+          typename H = std::hash<T>,                // hasher
+          typename E = std::equal_to<T>,            // equality comparator
+          typename A = std::allocator<Bucket<T>>    // allocator   
+         > struct RobinHoodHashTable
+{
+    typedef T value_type;
+
+    constexpr static float LOAD_FACTOR           = 0.7; // load factor
+    constexpr static const float EXP_GROWTH      = 2.0; // exponential growth factor
+    constexpr static const std::size_t INIT_SIZE =  64; // number of buckets to start with
+
+    /**
+     * Reserve more memory and rehash the table accordingly
+     */
+    void rehash()
+    {
+        //reserve the new capacity
+        _buckets.reserve(_capacity *= EXP_GROWTH);
+ 
+        //Each non-empty bucket is marked empty before its value is rehashed
+        for (const auto& b : _buckets)
+        {
+            if (!b.isEmpty())
+            {
+                b.markEmpty();
+                this->insert(b._value); 
+            }
+            //The filled buckets cannot be futher than the old capacity
+            if ((&b - &_buckets[0]) * EXP_GROWTH > _capacity )
+            {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Insert an element in the hashtable (!by copy!)
+     */
+    void insert(value_type t)
+    {
+        //Check if one need a rehash
+        if ((++_size) > _capacity * LOAD_FACTOR)
+        {
+            rehash();
+        }
+
+        //Robin Hood hashing technique
+        std::size_t hash = _hasher(t);       
+        std::size_t dib = 0;
+        Bucket<T>* head = &_buckets[hash];
+
+        do
+        {
+            while (!(head->isEmpty()) &&
+                     dib < head->_dib)
+            {
+                head++;
+                dib++;
+                if (head - &_buckets[0] == _buckets.size())
+                {
+                    head = &_buckets[0]; 
+                }
+            }
+
+            const T tTmp = _hasher(head->_value);
+            const std::size_t dibTmp = head->_dib;
+       
+            head->_value = t;
+            head->_dib = dib;
+
+            t = tTmp;
+            dib = dibTmp;
+            
+        } while (dib != Bucket<T>::EMPTY);
+
+    }
+
+    std::size_t size() { return _size; }
+
+    std::vector<Bucket<T>>  _buckets;
+    static const H          _hasher;
+    std::size_t             _size;
+    std::size_t             _capacity = INIT_SIZE;
+};
+
+#endif
