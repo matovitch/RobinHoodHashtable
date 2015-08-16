f9e03f64bb8101762c8195dd732ffcabf11f41c5
cbrugel <camille.brugel@laposte.net>
2015-08-16 21:05:18 +0200

    [WIP] dib as uint8_t + main toy example

--- RobinHoodHashTable.hpp
+++ RobinHoodHashTable.hpp
@@ -19,23 +19,23 @@ struct Bucket
 {
     typedef T value_type;
 
-    constexpr static std::size_t EMPTY  = static_cast<std::size_t>(-1);
+    constexpr static std::size_t EMPTY  = static_cast<uint8_t>(-1);
 
     void markEmpty() { _dib = EMPTY; }
     bool isEmpty() const { return _dib == EMPTY; }
 
-    std::size_t _dib = EMPTY;   // Distance to Initial Bucket
-    T           _value;         // the actual value
+    uint8_t _dib = EMPTY;   // Distance to Initial Bucket
+    T       _value;         // the actual value
 };
 
 
-typedef int T; //TODO comment
+//typedef int T; //TODO comment
 
 
 /**
  * RobinHoodHashTable: hashtable using the Robin Hood hashing
  */
-template <// TODO uncomment typename T,                               // type of the contained values
+template <typename T,                               // type of the contained values
           typename H = std::hash<T>,                // hasher
           typename E = std::equal_to<T>,            // equality comparator
           typename A = std::allocator<Bucket<T>>    // allocator   
@@ -47,6 +47,11 @@ template <// TODO uncomment typename T,                               // type of
     constexpr static const float EXP_GROWTH      = 2.0; // exponential growth factor
     constexpr static const std::size_t INIT_SIZE =  64; // number of buckets to start with
 
+    RobinHoodHashTable() : _size(0),
+                           _hasher(),
+                           _buckets(INIT_SIZE),
+                           _capacity(INIT_SIZE) {}
+
     /**
      * Reserve more memory and rehash the table accordingly
      */
@@ -56,7 +61,7 @@ template <// TODO uncomment typename T,                               // type of
         _buckets.reserve(_capacity *= EXP_GROWTH);
  
         //Each non-empty bucket is marked empty before its value is rehashed
-        for (const auto& b : _buckets)
+        for (auto& b : _buckets)
         {
             if (!b.isEmpty())
             {
@@ -83,14 +88,14 @@ template <// TODO uncomment typename T,                               // type of
         }
 
         //Robin Hood hashing technique
+        uint8_t dib = 0;
         std::size_t hash = _hasher(t);       
-        std::size_t dib = 0;
-        Bucket<T>* head = &_buckets[hash];
+        Bucket<T>* head = &_buckets[hash % _buckets.size()];
 
         do
         {
             while (!(head->isEmpty()) &&
-                     dib < head->_dib)
+                   dib < head->_dib)
             {
                 head++;
                 dib++;
@@ -101,7 +106,7 @@ template <// TODO uncomment typename T,                               // type of
             }
 
             const T tTmp = _hasher(head->_value);
-            const std::size_t dibTmp = head->_dib;
+            const uint8_t dibTmp = head->_dib;
        
             head->_value = t;
             head->_dib = dib;
@@ -116,9 +121,10 @@ template <// TODO uncomment typename T,                               // type of
     std::size_t size() { return _size; }
 
     std::vector<Bucket<T>>  _buckets;
-    static const H          _hasher;
+    std::size_t             _capacity;
     std::size_t             _size;
-    std::size_t             _capacity = INIT_SIZE;
+    const H                 _hasher;
 };
+
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "RobinHoodHashTable.hpp"
+
+int main()
+{
+    RobinHoodHashTable<int> r;
+
+    r.insert(1);
+
+    return 0;
+}
